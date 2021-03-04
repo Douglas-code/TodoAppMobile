1964bd6dfb0ee213a12c6f77f0db5095bbcd223c
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2021-03-03 21:53:08 -0300

    adicionando metodo para remover item

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,12 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,14 +76,22 @@ class _HomePageState extends State<HomePage> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
-            title: Text(item.title),
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
             key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
+            background: Container(
+              color: Colors.red,
+            ),
+            onDismissed: (direction) {
+              remove(index);
             },
           );
         },
